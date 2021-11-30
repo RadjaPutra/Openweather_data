358b4b4e1f9543cb67e50dcf9094fd2f7c1b4d23
RadjaPutra <89650085+RadjaPutra@users.noreply.github.com>
2021-12-01 00:50:42 +0800

    Create NTB.py

---
+++ NTB.py
@@ -0,0 +1,25 @@
+import sys
+!{sys.executable} -m pip install kafka-python
+import time
+import json
+from json import dumps
+from kafka import KafkaProducer
+from time import sleep
+import requests as req
+
+#call_the_API
+NTB="http://api.openweathermap.org/data/2/5/weather?id=1633792&appid=6afa72ee728492b6960489dfba7a472a&units=metric"
+brokers='localhost:9092'
+topic='weather_topic'
+sleep_time=5
+
+#declare_the_producer
+producer = KafkaProducer(bootstrap_servers=[brokers],value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+#getting_the_data_send_to_consumer
+while(True):
+  print("Getting new data..")
+  resp = req.get(NTB)
+  json_data = json.loads(resp.text)
+  producer.send(topic, json_Data)
+  time.sleep(sleep_time)
